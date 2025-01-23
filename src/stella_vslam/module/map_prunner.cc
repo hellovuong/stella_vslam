4774c8cb71c6842d520928632b6c58041be67f32
Long Vuong <vuong@wheel.me>
2025-01-23 14:57:01 +0100

    introduce landmark prune

--- src/stella_vslam/module/map_prunner.cc
+++ src/stella_vslam/module/map_prunner.cc
@@ -119,6 +119,8 @@ void map_prunner::run() {
         }
     }
 
+    clean_up_landmarks();
+
     spdlog::info("Before prune: total kfs {} and lms {}", num_kfs_before, num_lms_before);
     spdlog::info("After prune:  total kfs {} and lms {}", map_db_->get_num_keyframes(), map_db_->get_num_landmarks());
 }
@@ -147,22 +149,177 @@ void map_prunner::delete_keyframes(const std::unordered_map<unsigned int, double
     }
 }
 
-            for (const auto& lm : v_landmarks) {
-                if (!lm) {
-                    continue;
-                }
-                if (lm->will_be_erased()) {
-                    continue;
-                }
-                if (!lm->has_representative_descriptor()) {
-                    lm->compute_descriptor();
-                }
-                if (!lm->has_valid_prediction_parameters()) {
-                    lm->update_mean_normal_and_obs_scale_variance();
-                }
-            }
+map_prunner::TriangulationResult map_prunner::triangulate_from_n_views(const Eigen::Matrix3Xd& t_G_bv, const Eigen::Matrix3Xd& p_G_C, Eigen::Vector3d* p_G_P) {
+    const int num_measurements = t_G_bv.cols();
+    if (num_measurements < 2) {
+        return TriangulationResult::TOO_FEW_MEASUREMENTS;
+    }
+
+    // 1.) Formulate the geometrical problem
+    // p_G_P + alpha[i] * t_G_bv[i] = p_G_C[i]      (+ alpha intended)
+    // as linear system Ax = b, where
+    // x = [p_G_P; alpha[0]; alpha[1]; ... ] and b = [p_G_C[0]; p_G_C[1]; ...]
+    //
+    // 2.) Apply the approximation AtAx = Atb
+    // AtA happens to be composed of mostly more convenient blocks than A:
+    // - Top left = N * Eigen::Matrix3d::Identity()
+    // - Top right and bottom left = t_G_bv
+    // - Bottom right = t_G_bv.colwise().squaredNorm().asDiagonal()
+
+    // - Atb.head(3) = p_G_C.rowwise().sum()
+    // - Atb.tail(N) = columnwise dot products between t_G_bv and p_G_C
+    //               = t_G_bv.cwiseProduct(p_G_C).colwise().sum().transpose()
+    //
+    // 3.) Apply the Schur complement to solve after p_G_P only
+    // AtA = [E B; C D] (same blocks as above) ->
+    // (E - B * D.inverse() * C) * p_G_P = Atb.head(3) - B * D.inverse() * Atb.tail(N)
+
+    const Eigen::MatrixXd BiD = t_G_bv * t_G_bv.colwise().squaredNorm().asDiagonal().inverse();
+    const Eigen::Matrix3d AxtAx = num_measurements * Eigen::Matrix3d::Identity() - BiD * t_G_bv.transpose();
+    const Eigen::Vector3d Axtbx = p_G_C.rowwise().sum() - BiD * t_G_bv.cwiseProduct(p_G_C).colwise().sum().transpose();
+
+    Eigen::ColPivHouseholderQR<Eigen::Matrix3d> qr = AxtAx.colPivHouseholderQr();
+    static constexpr double kRankLossTolerance = 1e-5;
+    qr.setThreshold(kRankLossTolerance);
+    const size_t rank = qr.rank();
+    if (rank < 3) {
+        return TriangulationResult::UNOBSERVABLE;
+    }
+
+    *p_G_P = qr.solve(Axtbx);
+    return TriangulationResult::SUCCESSFUL;
+}
+
+bool map_prunner::is_well_constrained_lm(std::shared_ptr<data::landmark> landmark) {
+    const auto& backlinks = landmark->get_observations();
+    if (backlinks.size() < min_observers) {
+        return false;
+    }
+
+    const Eigen::Vector3d& p_G_fi = landmark->get_pos_in_world();
+    std::vector<Eigen::Vector3d> G_normalized_incidence_rays;
+    G_normalized_incidence_rays.reserve(backlinks.size());
+    double signed_distance_from_closest_observer = std::numeric_limits<double>::max();
+    for (const auto& [ptr, id] : backlinks) {
+        const auto& vertex = ptr.lock();
+
+        // p_C = R_cw * p_w + t_cw
+        // p_W = R_wc * pc + t_wc
+        const Eigen::Vector3d p_C_fi = vertex->get_rot_cw() * landmark->get_pos_in_world() + vertex->get_trans_cw();
+        const Eigen::Vector3d G_incidence_ray = vertex->get_trans_wc() - p_G_fi;
+
+        const double distance = G_incidence_ray.norm();
+        const double signed_distance = distance * (p_C_fi(2) < 0.0 ? -1.0 : 1.0);
+        signed_distance_from_closest_observer = std::min(signed_distance_from_closest_observer, signed_distance);
+
+        if (distance > 0) {
+            G_normalized_incidence_rays.emplace_back(G_incidence_ray / distance);
+        }
+        auto undist_keypt = vertex->frm_obs_.undist_keypts_.at(id);
+        const auto scale_level = static_cast<unsigned int>(undist_keypt.octave);
+        // Reproject and compute visibility
+        Vec2_t reproj;
+        float x_right;
+        const bool in_image = vertex->camera_->reproject_to_image(vertex->get_rot_cw(), vertex->get_trans_cw(), landmark->get_pos_in_world(), reproj, x_right);
+        if (!in_image) {
+            return false;
+        }
+        Vec2_t kpt{undist_keypt.pt.x, undist_keypt.pt.y};
+        auto reprojection_error_px_sq = (reproj - kpt).squaredNorm();
+
+        constexpr float chi_sq_3D = 7.81473;
+        if (chi_sq_3D < reprojection_error_px_sq * vertex->orb_params_->inv_level_sigma_sq_.at(scale_level)) {
+            return false;
         }
     }
+
+    if (signed_distance_from_closest_observer > max_distance_from_closest_observer || signed_distance_from_closest_observer < min_distance_from_closest_observer) {
+        return false;
+    }
+
+    const double max_disparity_angle_rad = getMaxDisparityRadAngleOfUnitVectorBundle(G_normalized_incidence_rays);
+
+    constexpr double kRadToDeg = 180.0 / M_PI;
+    double angle_deg = max_disparity_angle_rad * kRadToDeg;
+    if (angle_deg < min_observation_angle_deg) {
+        return false;
+    }
+
+    return true;
+}
+
+void map_prunner::clean_up_landmarks() {
+    TriangulationResult ret;
+    const auto& all_landmarks = map_db_->get_all_landmarks();
+
+    for (auto lm : all_landmarks) {
+        if (!lm) {
+            continue;
+        }
+
+        if (lm->will_be_erased()) {
+            continue;
+        }
+        // The following have one entry per measurement:
+        Eigen::Matrix3Xd G_bearing_vectors;
+        Eigen::Matrix3Xd p_G_C_vectors;
+        Eigen::Matrix3Xd p_G_fi_vectors;
+
+        // [kf, id]
+        auto observations = lm->get_observations();
+
+        if (observations.size() < 2) {
+            lm->prepare_for_erasing(map_db_);
+            continue;
+        }
+
+        G_bearing_vectors.conservativeResize(Eigen::NoChange, observations.size());
+        p_G_C_vectors.conservativeResize(Eigen::NoChange, observations.size());
+
+        int num_measurements = 0;
+        for (auto& [kf, id] : observations) {
+            auto C_bearing_vector = kf.lock()->frm_obs_.bearings_.at(id);
+            auto G_bearing = kf.lock()->get_rot_cw().transpose() * C_bearing_vector;
+            G_bearing_vectors.col(num_measurements) = G_bearing;
+            p_G_C_vectors.col(num_measurements) = kf.lock()->get_trans_wc();
+            num_measurements++;
+        }
+        // Resize to final number of valid measurements
+        G_bearing_vectors.conservativeResize(Eigen::NoChange, num_measurements);
+        p_G_C_vectors.conservativeResize(Eigen::NoChange, num_measurements);
+
+        // return result
+        Eigen::Vector3d p_G_fi;
+        ret = triangulate_from_n_views(G_bearing_vectors, p_G_C_vectors, &p_G_fi);
+
+        if (ret == TriangulationResult::SUCCESSFUL) {
+            lm->set_pos_in_world(p_G_fi);
+            lm->update_mean_normal_and_obs_scale_variance();
+            if (!is_well_constrained_lm(lm)) {
+                lm->prepare_for_erasing(map_db_);
+            }
+        }
+        else {
+            lm->prepare_for_erasing(map_db_);
+        }
+    }
+}
+
+double map_prunner::getMaxDisparityRadAngleOfUnitVectorBundle(const std::vector<Eigen::Vector3d>& unit_incidence_rays) {
+    if (unit_incidence_rays.size() < 2u) {
+        return 0.0;
+    }
+
+    double min_cos_angle = 1.0;
+    for (size_t i = 0; i < unit_incidence_rays.size(); ++i) {
+        for (size_t j = i + 1; j < unit_incidence_rays.size(); ++j) {
+            assert(std::abs(unit_incidence_rays[i].squaredNorm() - 1.0) <= 1e-6 && std::abs(unit_incidence_rays[i].squaredNorm() - 1.0) <= 1e-6);
+            min_cos_angle = std::min(
+                min_cos_angle,
+                std::abs(unit_incidence_rays[i].dot(unit_incidence_rays[j])));
+        }
+    }
+    return std::acos(min_cos_angle);
 }
 } // namespace module
 } // namespace stella_vslam
--- src/stella_vslam/module/map_prunner.h
+++ src/stella_vslam/module/map_prunner.h
@@ -35,13 +35,43 @@ private:
     // Memeber methods for keyframes pruning
 
     /**
-     * @brief Select views (keyframes) to be deleted
+     * @brief Algorithm for managing keyframes (views) in a visual SLAM or mapping system.
      *
-     * @param V set of views to be checked
+     * This algorithm identifies and removes redundant views (keyframes) from the map
+     * to optimize storage and computation while maintaining the performance of the system.
+     * It evaluates the importance of views based on observation statistics, relocalization
+     * usefulness, and spatial distribution.
      *
-     * @return set of view to be deleted [id, score]
+     * @details
+     * The algorithm operates as follows:
+     * - If the total number of views is less than or equal to a predefined minimum
+     *   (`MIN_VIEWS`), no views are deleted.
+     * - A subset of views to keep (`V_keep`) is initialized with views created in the
+     *   current run that have been observed at least once.
+     * - For each remaining view, a score is computed based on:
+     *   - `reloc`: Whether the view was used for relocalization.
+     *   - `n_obs_cur / max_obs`: The ratio of the number of observations in the current run
+     *     to the maximum number of observations across all views.
+     *   - `n_obs_runs / n_runs`: The fraction of runs in which the view was observed.
+     *   Each factor is weighted by predefined constants (`W1`, `W2`, `W3`).
+     * - Views with scores exceeding a predefined threshold (`SCORE_THRESHOLD`) are added to `V_keep`.
+     * - Views not in `V_keep` are considered for deletion. These views are sorted by score
+     *   in ascending order.
+     * - To ensure a uniform spatial distribution, each view in the deletion set is checked
+     *   for the number of spatially nearby neighbors (`numNearestNeighbors`). If the number
+     *   of neighbors is below a threshold (`NN_THRESHOLD`), the view is retained.
+     * - The final set of views for deletion (`V_delete`) is returned.
+     *
+     * @param V A set of all views (keyframes) in the map.
+     * @param MIN_VIEWS The minimum number of views required to avoid pruning.
+     * @param SCORE_THRESHOLD The score threshold to decide which views to keep.
+     * @param W1 Weight for the relocalization factor in the score.
+     * @param W2 Weight for the current observation factor in the score.
+     * @param W3 Weight for the run observation factor in the score.
+     * @param NN_THRESHOLD Minimum number of spatially nearby views required for a view to be deleted.
      *
      * @note https://arxiv.org/pdf/1908.03605
+     * @return V_delete A set of views to delete based on the above criteria.
      */
     std::unordered_map<unsigned int, double> select_view_to_prune(std::unordered_map<unsigned int, std::shared_ptr<data::keyframe>>& V);
 
@@ -51,6 +81,118 @@ private:
      * @param D
      */
     void delete_keyframes(const std::unordered_map<unsigned int, double>& D);
+
+    //-----------------------------------------
+    // Memeber methods for landmark pruning
+    enum class TriangulationResult {
+        SUCCESSFUL,
+        UNOBSERVABLE,
+        TOO_FEW_MEASUREMENTS
+    };
+
+    /**
+     * @brief Clean up weak landmarks
+     */
+    void clean_up_landmarks();
+
+    /// brief Triangulate a 3d point from a set of n keypoint measurements as
+    ///       bearing vectors.
+    /// @param t_G_bv Back-projected bearing vectors from visual frames to
+    ///               observations, expressed in the global frame.
+    /// @param p_G_C Global positions of visual frames (cameras).
+    /// @param p_G_P Triangulated point in global frame.
+    /// @return Was the triangulation successful?
+    //-----------------------------------------
+    TriangulationResult triangulate_from_n_views(const Eigen::Matrix3Xd& t_G_bv, const Eigen::Matrix3Xd& p_G_C, Eigen::Vector3d* p_G_P);
+
+    /**
+     * @brief Determines if a visual landmark is well-constrained based on
+     *        its observations and geometric properties.
+     *
+     * This method evaluates the quality of a given visual landmark in the map
+     * by verifying its constraints from multiple observations. A landmark is
+     * considered well-constrained if:
+     *
+     * 1. It is observed by a sufficient number of unique keyframes (backlinks).
+     * 2. The reprojection error of its position in the camera frame is within
+     *    an acceptable threshold for all observations.
+     * 3. The landmark's distance from its closest observer lies within a
+     *    defined range.
+     * 4. The angular disparity (disparity angle) of incidence rays from
+     *    different observing cameras is large enough to ensure triangulation
+     *    accuracy.
+     *
+     * @param[in] map The VIMap object containing the map data, including
+     *                vertices, observations, and transformations.
+     * @param[in] landmark The landmark to evaluate, containing its position,
+     *                     observations, and other properties.
+     *
+     * @return True if the landmark is well-constrained, false otherwise.
+     *
+     * ### Constraints Evaluated
+     * - **Number of Observations**:
+     *   - The landmark must be observed by at least `FLAGS_elq_min_observers`
+     *     cameras.
+     * - **Reprojection Error**:
+     *   - For each observation, the reprojection error (squared) in pixels must
+     *     not exceed `FLAGS_elq_max_reprojection_error_px^2`.
+     * - **Distance Constraints**:
+     *   - The signed distance of the landmark from its closest observing camera
+     *     must be within the range `[FLAGS_elq_min_distance_from_closest_observer,
+     *     FLAGS_elq_max_distance_from_closest_observer]`.
+     * - **Angular Disparity**:
+     *   - The maximum angular disparity between normalized incidence rays
+     *     from observing cameras must exceed `FLAGS_elq_min_observation_angle_deg`.
+     *
+     * ### Mathematical Overview
+     * 1. For each observation:
+     *    - Compute the normalized incidence ray \( \vec{r}_i = \frac{p_G - p_{G_C}}{\|p_G - p_{G_C}\|} \),
+     *      where \( p_G \) is the landmark position in the global frame and \( p_{G_C} \)
+     *      is the position of the observing camera in the global frame.
+     *    - Check the reprojection error of the observed keypoint.
+     *    - Update the signed distance to the closest observing camera.
+     * 2. After processing all observations:
+     *    - Compute the maximum disparity angle between incidence rays.
+     *    - Verify that the distance and angle constraints are satisfied.
+     *
+     */
+    bool is_well_constrained_lm(std::shared_ptr<data::landmark>);
+
+    /**
+     * @brief Computes the maximum angular disparity (in radians) between a bundle of unit incidence rays.
+     *
+     * This method calculates the largest angular separation between pairs of unit vectors
+     * (incidence rays) in a given bundle. The angular disparity is computed based on the
+     * cosine of the angle between the vectors and is returned in radians.
+     *
+     * @details
+     * The algorithm iterates through all unique pairs of unit vectors in the input and
+     * calculates the absolute cosine of the angle between each pair:
+     * - The cosine of the angle between two vectors, \f$ \mathbf{u} \f$ and \f$ \mathbf{v} \f$,
+     *   is given by:
+     *   \f[
+     *   \cos(\theta) = |\mathbf{u} \cdot \mathbf{v}|
+     *   \f]
+     * - The minimum cosine value (i.e., the smallest similarity between any pair) is tracked.
+     * - Finally, the angle corresponding to this minimum cosine is calculated as:
+     *   \f[
+     *   \theta = \arccos(\text{min\_cos\_angle})
+     *   \f]
+     *
+     * If the input bundle contains fewer than 2 unit vectors, the function returns 0.0
+     * since no meaningful disparity can be computed.
+     *
+     * @param unit_incidence_rays A vector of unit vectors (\f$ \mathbf{u} \f$), where each
+     * vector represents a direction in 3D space.
+     *
+     * @return The maximum angular disparity (in radians) between the unit vectors in the bundle.
+     * If there are fewer than 2 vectors, the return value is 0.0.
+     *
+     * @note The input vectors should be normalized (i.e., have a magnitude of 1) to ensure
+     * correct results. Non-normalized vectors may lead to incorrect angular computations.
+     */
+    double getMaxDisparityRadAngleOfUnitVectorBundle(const std::vector<Eigen::Vector3d>& unit_incidence_rays);
+
     // Memeber to get Views from map
 
     //! map database
@@ -60,7 +202,7 @@ private:
     data::bow_database* bow_db_ = nullptr;
 
     //-----------------------------------------
-    // Pruning Parameters - Sections IV.A
+    // Pruning keyframes parameters - Sections IV.A
 
     //! minimum number of views required before the pruning alg. to run
     unsigned int min_views_;
@@ -82,6 +224,13 @@ private:
 
     //! Weight total runs where the view was obeserved at least once
     double w3_;
+
+    //-----------------------------------------
+    // Pruning landmark parameters
+    static constexpr uint32_t min_observers = 4u;
+    static constexpr double min_distance_from_closest_observer = 0.05;
+    static constexpr double max_distance_from_closest_observer = 10;
+    static constexpr double min_observation_angle_deg = 5;
 };
 } // namespace module
 } // namespace stella_vslam
